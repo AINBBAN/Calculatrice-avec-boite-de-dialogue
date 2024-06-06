604b362c828edd5124a9dd466e49c98ba289b0d4
samy meddad <125839874+AINBBAN@users.noreply.github.com>
2024-06-06 17:23:17 +0200

    Update Calculatrice.js
    Ajout l'option de recommencer

--- Nouveau dossier/Calculatrice.js
+++ Nouveau dossier/Calculatrice.js
@@ -13,40 +13,43 @@ function Division(Nombre1,Nombre2) {
   }
     return Nombre1 / Nombre2;
 }
+let restart = false;
+do  {
 
+  do {
+      var Choix = Number(prompt("Que souhaitez-vous faire ? \n\n 1- Addition \n 2- Multiplication \n 3- Soustraction \n 4-Division"))
 
-do {
-    var Choix = Number(prompt("Que souhaitez-vous faire ? \n\n 1- Addition \n 2- Multiplication \n 3- Soustraction \n 4-Division"))
+  } while (Choix != 1 && Choix != 2 && Choix != 3 && Choix != 4) 
+  do {
+      var Nombre1 = Number(prompt("Entrez le premier nombre"))
+      var Nombre2 = Number(prompt("Entrez le deuxième nombre"))
 
-} while (Choix != 1 && Choix != 2 && Choix != 3 && Choix != 4) 
-do {
-    var Nombre1 = Number(prompt("Entrez le premier nombre"))
-    var Nombre2 = Number(prompt("Entrez le deuxième nombre"))
+  } while (isNaN(Nombre1) || isNaN(Nombre2));
 
-} while (isNaN(Nombre1) || isNaN(Nombre2));
-
-try{ 
-switch (Choix) {
-    case  1: 
-    var resultat = Addition(Nombre1,Nombre2);
+  try{ 
+  switch (Choix) {
+      case  1: 
+        var resultat = Addition(Nombre1,Nombre2);
+          break;
+      case  2:
+        var resultat = Multiplication(Nombre1,Nombre2);
+          break;
+      case   3 :
+        var resultat = Soustraction(Nombre1,Nombre2);
         break;
-    case  2:
-    var resultat = Multiplication(Nombre1,Nombre2);
-        break;
-    case   3 :
-    var resultat = Soustraction(Nombre1,Nombre2);
-        break;
-    case   4 :
-    var resultat = Division(Nombre1,Nombre2);   
-        break;
-     default:
+      case   4 :
+        var resultat = Division(Nombre1,Nombre2);   
+          break;
+       default:
             throw new Error("Il y'a une erreur.");
 }
-  alert("Le resultat est " + resultat);
+        alert("Le resultat est " + resultat);
 }
-catch(error){
-  alert(error);
+    catch(error){
+        alert(error);
 }
+  restart = confirm("Voulez-vous recomencer ?");
+}while(restart);
 
 
 
